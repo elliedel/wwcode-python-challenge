9840a8499177a11771484b23be992c31766423e9
Ellaine Dela Cruz <75776846+elliedel@users.noreply.github.com>
2024-01-27 22:25:28 +0800

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,8 @@
+''' 
+Day 17 - Create a program that capitalizes the first letter of each word in a sentence.
+'''
+
+sentence = input("Enter your sentence: ")
+
+# prints all the words in the string in titlecase.
+print("", sentence.title(), sep="\n")
